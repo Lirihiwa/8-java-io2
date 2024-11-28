2a7b4b61c80aabbe017ce28c6bd4e972d719e12c
Lirihiwa <lirihiwa@gmail.com>
2024-11-29 03:10:44 +0500

    Task 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,34 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
         System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
 
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
         // your implementation here
+        List<Path> allFiles = new ArrayList<>();
 
-        return null;
+        try (Stream<Path> entries = Files.walk(rootDir)) {
+            allFiles = entries
+                    .filter(Files::isRegularFile)
+                    .collect(Collectors.toCollection(ArrayList::new));
+        } catch (IOException e) {
+            throw new IOException("Ошибка при обходе каталога: " + rootDir, e);
+        }
+        return allFiles;
     }
 }
